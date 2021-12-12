3ebab04144461fa7e73570152e732a6d14b85b85
Shashank v <91933133+ShashankV2604@users.noreply.github.com>
2021-12-12 13:13:45 +0530

    added files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+function cal(){
+    let pr = document.getElementById("principal").value;
+let ti = document.getElementById ("years").value;
+let ra = document.getElementById ("rate").value;
+let text= document.getElementById("result");
+
+if(pr==0 && ti==0 && ra ==0){
+    text.innerHTML="Try input number in the field"
+}
+else{  
+    let si = (pr*ti*ra/100);
+    let time;
+    text.innerHTML =`If u deposit ${pr}, at an interest rate of ${ra}%. You will receive an amount of ${si},in the year ${time}`;
+}
 }
         
